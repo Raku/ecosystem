f8b11bad86d9281f6cb80d7955d51684c78df24c
Moritz Lenz <moritz@faui2k3.org>
2014-12-11 19:06:08 +0100

    [server] avoid temporary file

--- server/updatelist.pl
+++ server/updatelist.pl
@@ -1,25 +1,29 @@
 BEGIN { $ENV{HTTPS_CA_FILE} = '/etc/ssl/certs/ca-certificates.crt' }
 use 5.010;
 use JSON::XS;
-use File::Slurp 'slurp';
-use LWP::Simple;
+use LWP::UserAgent;
 use autodie;
 system "wget https://raw.githubusercontent.com/perl6/ecosystem/master/META.list -O metalist";
 
 my $OUTFILE = shift(@ARGV) // "/home/tjs/modules/public/projects.json";
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
 
 my @modules;
 
 open my $fh, '<', "metalist";
-for(<$fh>) {
-    chomp;
+for my $url (<$fh>) {
+    chomp $url;
     eval {
-        print "$_ ";
-        say getstore($_, 'tmp');
-        my $hash = decode_json slurp 'tmp';
-        push @modules, $hash;
+        print "$url ";
+        my $response = $ua->get($url);
+        say $response->code;
+        if ($response->is_success) {
+            my $hash = decode_json $response->content;
+            push @modules, $hash;
+        }
     };
-    unlink 'tmp' if -e 'tmp';
+    warn $@ if $@;
 }
 close $fh;
 #unlink 'metalist';
