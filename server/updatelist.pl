c528917991be47c4a8ab3b8a4baef1ea25e6cb83
Moritz Lenz <moritz@faui2k3.org>
2014-12-11 18:48:40 +0100

    [server] reduce dependencies

--- server/updatelist.pl
+++ server/updatelist.pl
@@ -2,7 +2,6 @@ BEGIN { $ENV{HTTPS_CA_FILE} = '/etc/ssl/certs/ca-certificates.crt' }
 use 5.010;
 use JSON::XS;
 use File::Slurp 'slurp';
-use Try::Tiny;
 use LWP::Simple;
 use autodie;
 system "wget https://raw.githubusercontent.com/perl6/ecosystem/master/META.list -O metalist";
@@ -12,7 +11,7 @@ my @modules;
 open my $fh, '<', "metalist";
 for(<$fh>) {
     chomp;
-    try {
+    eval {
         print "$_ ";
         say getstore($_, 'tmp');
         my $hash = decode_json slurp 'tmp';
